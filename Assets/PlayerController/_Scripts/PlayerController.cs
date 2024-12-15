50627d1d1cb7159b105c18f51c7974c8ace9af92
WakaWakaArdaAbi37 <cinarmert490@gmail.com>
2024-12-16 00:56:59 +0300

    Slope Handling added! (eğimde hareket eklendi.)

--- Assets/PlayerController/_Scripts/PlayerController.cs
+++ Assets/PlayerController/_Scripts/PlayerController.cs
@@ -22,6 +22,7 @@ namespace MTProject.PlayerController
         public float runSpeed = 4f;
         public float sprintAcceleration = 50f;
         public float sprintSpeed = 7f;
+        public float inAirAcceleration = .15f;
         public float drag = 20f;
         public float gravity = 25f;
         public float jumpSpeed = 1.0f;
@@ -36,15 +37,22 @@ namespace MTProject.PlayerController
         public float lookSenseV = 0.1f;
         public float lookLimitV = 89f;
 
+        [Header("Enviroment")] 
+        [SerializeField]private LayerMask _groundLayer;
+
         private PlayerLocomotionInput _playerLocomotionInput;
         private PlayerState _playerState;
 
         private Vector2 _cameraRotation = Vector2.zero;
         private Vector2 _playerTargetRotation = Vector2.zero;
 
+        private bool _jumpedLastFrame = false;
         private bool _isRotatingClockwise = false;
         private float _rotatingToTargetTimer = 0f;
         private float _verticalVelocity = 0f;
+        private float _antiBump;
+        private float _stepOffSet;
+        
         #endregion
 
         #region Startup
@@ -52,6 +60,9 @@ namespace MTProject.PlayerController
         {
             _playerLocomotionInput = GetComponent<PlayerLocomotionInput>();
             _playerState = GetComponent<PlayerState>();
+
+            _antiBump = sprintSpeed;
+            _stepOffSet = _characterController.stepOffset;
         }
         #endregion
 
@@ -79,29 +90,39 @@ namespace MTProject.PlayerController
             _playerState.SetPlayerMovementState(lateralState);
 
             // Control Airborn State
-            if (!isGrounded && _characterController.velocity.y > 0f)
+            if ((!isGrounded||_jumpedLastFrame) && _characterController.velocity.y > 0f)
             {
                 _playerState.SetPlayerMovementState(PlayerMovementState.Jumping);
+                _jumpedLastFrame = false;
+                _characterController.stepOffset = 0;
             }
-            else if (!isGrounded && _characterController.velocity.y <= 0f)
+            else if ((!isGrounded||_jumpedLastFrame) && _characterController.velocity.y <= 0f)
             {
                 _playerState.SetPlayerMovementState(PlayerMovementState.Falling);
+                _jumpedLastFrame = false;
+                _characterController.stepOffset = 0;
+            }
+            else
+            {
+                _characterController.stepOffset = _stepOffSet;
             }
         }
 
         private void HandleVerticalMovement()
         {
             bool isGrounded = _playerState.InGroundedState();
+            
+            _verticalVelocity -= gravity * Time.deltaTime;
 
             if (isGrounded && _verticalVelocity < 0)
-                _verticalVelocity = 0f;
-
-            _verticalVelocity -= gravity * Time.deltaTime;
+                _verticalVelocity = -_antiBump;
 
             if (_playerLocomotionInput.JumpPressed && isGrounded)
             {
-                _verticalVelocity += Mathf.Sqrt(jumpSpeed * 3 * gravity);
+                _verticalVelocity += _antiBump+Mathf.Sqrt(jumpSpeed * 3 * gravity);
+                _jumpedLastFrame = true;
             }
+            
         }
 
         private void HandleLateralMovement()
@@ -112,9 +133,12 @@ namespace MTProject.PlayerController
             bool isWalking = _playerState.CurrentPlayerMovementState == PlayerMovementState.Walking;
 
             // State dependent acceleration and speed
-            float lateralAcceleration = isWalking ? walkAcceleration :
+            float lateralAcceleration = !isGrounded?inAirAcceleration:
+                                        isWalking ? walkAcceleration :
                                         isSprinting ? sprintAcceleration : runAcceleration;
-            float clampLateralMagnitude = isWalking ? walkSpeed :
+            
+            float clampLateralMagnitude = !isGrounded ? sprintSpeed:
+                                          isWalking ? walkSpeed :
                                           isSprinting ? sprintSpeed : runSpeed;
 
             Vector3 cameraForwardXZ = new Vector3(_playerCamera.transform.forward.x, 0f, _playerCamera.transform.forward.z).normalized;
@@ -127,7 +151,7 @@ namespace MTProject.PlayerController
             // Add drag to player
             Vector3 currentDrag = newVelocity.normalized * drag * Time.deltaTime;
             newVelocity = (newVelocity.magnitude > drag * Time.deltaTime) ? newVelocity - currentDrag : Vector3.zero;
-            newVelocity = Vector3.ClampMagnitude(newVelocity, clampLateralMagnitude);
+            newVelocity = Vector3.ClampMagnitude(new Vector3(newVelocity.x,0,newVelocity.z), clampLateralMagnitude);
             newVelocity.y += _verticalVelocity;
 
             // Move character (Unity suggests only calling this once per tick)
@@ -206,6 +230,23 @@ namespace MTProject.PlayerController
         }
 
         private bool IsGrounded()
+        {
+            bool grounded = _playerState.InGroundedState() ? IsGroundedWhileGrounded() : IsGroundedWhileAirborne();
+
+            return grounded;
+        }
+
+        private bool IsGroundedWhileGrounded()
+        {
+            Vector3 spherePosition = new Vector3(transform.position.x,
+                transform.position.y - _characterController.radius, transform.position.z);
+
+            bool grounded = Physics.CheckSphere(spherePosition, _characterController.radius, _groundLayer,QueryTriggerInteraction.Ignore);
+
+            return grounded;
+        }
+
+        private bool IsGroundedWhileAirborne()
         {
             return _characterController.isGrounded;
         }
